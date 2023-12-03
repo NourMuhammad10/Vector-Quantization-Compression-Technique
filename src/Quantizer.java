0d372cdca9d916b802e1e70011273520b5c64eb9
Youssef Moataz <yousefcrunshy03@gmail.com>
2023-12-03 19:33:52 +0200

    Added comments to compression and its helper functions.

--- src/Quantizer.java
+++ src/Quantizer.java
@@ -5,13 +5,14 @@ import java.util.Vector;
 
 public class Quantizer {
     public void compress(String inputFilePath, String outputFilePath) throws FileNotFoundException {
-        String originalStream = "";
+        StringBuilder originalStream = new StringBuilder();
         Integer numLines = 0;
         try {
             BufferedReader reader = new BufferedReader(new FileReader(inputFilePath));
+            // read file to string and count lines
             while (reader.ready()) {
-                originalStream += reader.readLine();
-                originalStream += '\n';
+                originalStream.append(reader.readLine());
+                originalStream.append('\n');
                 numLines++;
             }
             reader.close();
@@ -19,31 +20,37 @@ public class Quantizer {
             e.printStackTrace();
         }
 
-        if (originalStream.equals("")) {
+        if (originalStream.toString().isEmpty()) {
             return;
         }
 
-        Integer numVectors = (int) Math.pow((double) numLines / 2, 2);
-        Integer vectorLength = 4;
         Integer codebookLength = 4;
+        Integer vectorHeight = 2;
+        Integer vectorWidth = 2;
+        Integer vectorLength = vectorWidth * vectorHeight;
+        Integer numVectors = (int) Math.pow((double) numLines, 2) / vectorLength;
 
-        String[] lines = originalStream.split("\n");
+        // extract vectors from strings
+        String[] lines = originalStream.toString().split("\n");
         List<String[]> initialVectors = new ArrayList<>();
 
         for (int i = 0; i < numLines; i++) {
             initialVectors.add(lines[i].split(" "));
         }
 
+        // build custom-sized vectors
         List<Vector<Double>> vectors = new ArrayList<>();
 
-        for (int i = 0; i < numLines; i += 2) {
-            for (int j = 0; j < numLines; j += 2) {
-                Vector<Double> currentVector = new Vector<>(vectorLength);
+        for (int i = 0; i < numLines; i += vectorHeight) {
+            for (int j = 0; j < numLines; j += vectorWidth) {
 
-                currentVector.add(Double.valueOf(initialVectors.get(i)[j]));
-                currentVector.add(Double.valueOf(initialVectors.get(i)[j + 1]));
-                currentVector.add(Double.valueOf(initialVectors.get(i + 1)[j]));
-                currentVector.add(Double.valueOf(initialVectors.get(i + 1)[j + 1]));
+                Vector<Double> currentVector = new Vector<>(vectorLength);
+                for (int k = 0; k < vectorHeight; k++) {
+                    for (int l = 0; l < vectorWidth; l++) {
+                        currentVector.add(Double.valueOf(initialVectors.get(i + k)[j + l]));
+
+                    }
+                }
 
                 vectors.add(currentVector);
             }
@@ -57,10 +64,12 @@ public class Quantizer {
 //            System.out.println(vectors.get(i) + " -> " + findClosestCodebookVector(vectors.get(i), codebook));
 //        }
 
+        // match labels to suitable vectors
         Vector<Integer> labels = new Vector<>();
         for (int i = 0; i < numVectors; i++) {
             labels.add(findClosestCodebookVector(vectors.get(i), codebook));
         }
+        // save the output
         BinaryFilesHandler.writeCompressedOutput(codebook, labels, outputFilePath);
 
     }
@@ -70,6 +79,7 @@ public class Quantizer {
         Integer numVectors = initialVectors.size();
         List<Vector<Double>> codebook = new ArrayList<>();
 
+        // calculate first average vector
         Vector<Double> codebookInitialVector = new Vector<>();
         for (int i = 0; i < initialVectors.get(0).size(); i += 1) {
             Double avg = 0.0;
@@ -78,13 +88,14 @@ public class Quantizer {
             }
             codebookInitialVector.add(avg / numVectors);
         }
-
         codebook.add(codebookInitialVector);
 
+        // loop till filling codebook
         while (codebook.size() < requiredSize) {
 
             List<Vector<Double>> updatedCodebook = new ArrayList<>();
 
+            // do the splitting for every codebook vector
             for (int i = 0; i < codebook.size(); i += 1) {
                 updatedCodebook.add(splitVector(codebook.get(i), -1.0));
                 updatedCodebook.add(splitVector(codebook.get(i), 1.0));
@@ -94,6 +105,7 @@ public class Quantizer {
 
             List<Vector<Vector<Double>>> quantized = quantize(initialVectors, updatedCodebook);
 
+            // calculate averages for every matched group separately
             for (int i = 0; i < quantized.size(); i++) {
 
                 Vector<Double> codeBookVector = new Vector<>();
@@ -113,12 +125,15 @@ public class Quantizer {
 
         List<Vector<Double>> updatedCodebook2;
 
+        // loop till no changed groups
         while (true) {
 
             updatedCodebook2 = new ArrayList<>();
 
             List<Vector<Vector<Double>>> quantized = quantize(initialVectors, codebook);
 
+            // calculate averages for every matched group separately
+            // same as previous loop
             for (int i = 0; i < quantized.size(); i++) {
 
                 Vector<Double> codeBookVector = new Vector<>();
@@ -133,6 +148,7 @@ public class Quantizer {
 
             }
 
+            // stop when no change
             if (codebook.equals(updatedCodebook2)) {
                 break;
             }
@@ -167,6 +183,7 @@ public class Quantizer {
             quantizedIndices.add(new Vector<>());
         }
 
+        // do the vector-codebook matching
         for (Vector<Double> inputVector : inputVectors) {
             int bestIndex = findClosestCodebookVector(inputVector, codebook);
             quantizedIndices.get(bestIndex).add(inputVector);
@@ -175,6 +192,7 @@ public class Quantizer {
     }
 
     private Integer findClosestCodebookVector(Vector<Double> inputVector, List<Vector<Double>> codebook) {
+        // find index of best matching codebook vector to the input vector
         Integer bestIndex = 0;
         Double bestDistance = Double.MAX_VALUE;
         for (int i = 0; i < codebook.size(); i++) {
@@ -196,10 +214,14 @@ public class Quantizer {
     }
 
     public static void main(String[] args) throws IOException {
-        String path = System.getProperty("user.dir") + "/compression" + ".bin";
+//        String path = System.getProperty("user.dir") + "/compression" + ".bin";
+//        Quantizer quantizer = new Quantizer();
+//        quantizer.compress("input.txt", path);
+//        quantizer.decompress(path);
+
         Quantizer quantizer = new Quantizer();
-        quantizer.compress("input.txt", path);
-        quantizer.decompress(path);
+        quantizer.compress("input.txt", "compression.bin");
+        quantizer.decompress("compression.bin");
     }
 
     public void decompress(String compressedFilePath) throws IOException {
@@ -225,18 +247,18 @@ public class Quantizer {
         for (int i = endOverHead; i < compressionValues.size(); i++) {
             Vector<Vector<Integer>> tempCodebook = new Vector<>();
             int temp = numBlockInRow;
-            while(temp != 0){
+            while (temp != 0) {
                 tempCodebook.add(codebook.get(compressionValues.get(i)));
                 i++;
                 temp--;
             }
             int times = blockDimension * blockDimension;
             int index = 0;
-            while(index < times) {
+            while (index < times) {
                 StringBuilder row = new StringBuilder();
                 for (int j = 0; j < tempCodebook.size(); j++) {
                     int temp2 = index;
-                    while (temp2 != (index + blockDimension)){
+                    while (temp2 != (index + blockDimension)) {
                         row.append(Integer.toString(tempCodebook.get(j).get(temp2)));
                         row.append(" ");
                         temp2++;
@@ -249,8 +271,8 @@ public class Quantizer {
         }
         File file = new File(System.getProperty("user.dir") + "/decompressedOutput" + ".txt");
         BufferedWriter writer = new BufferedWriter(new FileWriter(file));
-        try{
-            for(String row: original){
+        try {
+            for (String row : original) {
                 StringBuilder sb = new StringBuilder(row);
                 sb.deleteCharAt(row.length() - 1);
                 writer.write(sb.toString());
