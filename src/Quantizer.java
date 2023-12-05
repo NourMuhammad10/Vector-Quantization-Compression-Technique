784a4bd8e993e6016ff022d5d3ded785edb14ec6
Youssef Moataz <yousefcrunshy03@gmail.com>
2023-12-05 20:23:07 +0200

    Added image compression.

--- src/Quantizer.java
+++ src/Quantizer.java
@@ -1,3 +1,6 @@
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -5,59 +8,69 @@ import java.util.Vector;
 
 public class Quantizer {
     public void compress(String inputFilePath, String outputFilePath) throws FileNotFoundException {
-        StringBuilder originalStream = new StringBuilder();
-        Integer numLines = 0;
+
+        Vector<Vector<Integer>> imageVector = null;
+        int width = 0;
+        int height = 0;
+
         try {
-            BufferedReader reader = new BufferedReader(new FileReader(inputFilePath));
-            // read file to string and count lines
-            while (reader.ready()) {
-                originalStream.append(reader.readLine());
-                originalStream.append('\n');
-                numLines++;
+            BufferedImage image = ImageIO.read(new File(inputFilePath));
+            width = image.getWidth();
+            height = image.getHeight();
+
+//            System.out.println(width);
+//            System.out.println(height);
+//            System.out.println();
+
+            imageVector = new Vector<>(height);
+
+            for (int i = 0; i < width; i++) {
+                imageVector.add(i, new Vector<>(height));
+                for (int j = 0; j < height; j++) {
+                    int rgb = image.getRGB(i, j);
+                    int red = (rgb >> 16) & 0xFF;
+                    int green = (rgb >> 8) & 0xFF;
+                    int blue = rgb & 0xFF;
+                    int gray = (red + green + blue) / 3;
+
+                    imageVector.get(i).add(j, (Integer) gray);
+                }
             }
-            reader.close();
-        } catch (Exception e) {
+
+        } catch (IOException e) {
             e.printStackTrace();
         }
 
-        if (originalStream.toString().isEmpty()) {
-            return;
-        }
-
-        Integer codebookLength = 4;
+        Integer codebookLength = 8;
         Integer vectorHeight = 2;
         Integer vectorWidth = 2;
-        Integer vectorLength = vectorWidth * vectorHeight;
-        Integer numVectors = (int) Math.pow((double) numLines, 2) / vectorLength;
-
-        // extract vectors from strings
-        String[] lines = originalStream.toString().split("\n");
-        List<String[]> initialVectors = new ArrayList<>();
-
-        for (int i = 0; i < numLines; i++) {
-            initialVectors.add(lines[i].split(" "));
-        }
+        int temp = width;
+        width = height;
+        height = temp;
 
         // build custom-sized vectors
-        List<Vector<Double>> vectors = new ArrayList<>();
+        Vector<Vector<Vector<Integer>>> vectors = new Vector<>();
 
-        for (int i = 0; i < numLines; i += vectorHeight) {
-            for (int j = 0; j < numLines; j += vectorWidth) {
-
-                Vector<Double> currentVector = new Vector<>(vectorLength);
-                for (int k = 0; k < vectorHeight; k++) {
-                    for (int l = 0; l < vectorWidth; l++) {
-                        currentVector.add(Double.valueOf(initialVectors.get(i + k)[j + l]));
-
+        for (int i = 0; i < height; i += vectorHeight) {
+            for (int j = 0; j < width; j += vectorWidth) {
+                Vector<Vector<Integer>> row = new Vector<>();
+                for (int k = i; k < i + vectorHeight  ; k++) {
+                    Vector<Integer> currentVector = new Vector<>();
+                    for (int l = j; l < j + vectorWidth  ; l++) {
+                        currentVector.add(imageVector.get(k).get(l));
                     }
+                    row.add(currentVector);
                 }
 
-                vectors.add(currentVector);
+                vectors.add(row);
             }
         }
 
-        List<Vector<Double>> codebook = buildCodebook(vectors, codebookLength);
+        Vector<Vector<Vector<Integer>>> codebook = buildCodebook(vectors, codebookLength);
+//        System.out.println(codebook.size());
+//        System.out.println(codebook);
 
+//        System.out.println(imageVector);
 //        System.out.println(codebook);
 //
 //        for (int i = 0; i < numVectors; i++) {
@@ -66,137 +79,196 @@ public class Quantizer {
 
         // match labels to suitable vectors
         Vector<Integer> labels = new Vector<>();
-        for (int i = 0; i < numVectors; i++) {
-            labels.add(findClosestCodebookVector(vectors.get(i), codebook));
+        for (Vector<Vector<Integer>> v : vectors) {
+            labels.add(findClosestCodebookVector(v, codebook));
         }
+
+//        System.out.println(labels.size());
         // save the output
-        BinaryFilesHandler.writeCompressedOutput(codebook, labels, outputFilePath);
+//        BinaryFilesHandler.writeCompressedOutput(codebook, labels, outputFilePath);
+
+        BufferedImage image = new BufferedImage(height, width, BufferedImage.TYPE_INT_RGB);
+        Graphics2D g2d = image.createGraphics();
+        g2d.setColor(Color.WHITE);
+        g2d.fillRect(0, 0, height, width);
+
+        int xSpacing = width / vectorWidth;
+        int ySpacing = height / vectorHeight;
+//        System.out.println(xSpacing);
+//        System.out.println(ySpacing);
+
+        int position = 0;
+
+        for (int i = 0; i < height; i += vectorHeight) {
+            for (int j = 0; j < width; j += vectorWidth) {
+
+                Vector<Vector<Integer>> currentVector = getVector(labels.get(position), codebook);
+                for (int k = 0; k < currentVector.size(); k++) {
+                    for (int l = 0; l < currentVector.get(k).size(); l++) {
+
+                        int label = currentVector.get(k).get(l);
+
+                        Color color = new Color(label, label, label);
+                        int pixelX = i + k;
+                        int pixelY = j + l;
+                        g2d.setColor(color);
+                        g2d.fillRect(pixelX, pixelY, 1, 1);
+
+                    }
+                }
+
+                position++;
+            }
+        }
+
+        g2d.dispose();
+
+        try {
+            ImageIO.write(image, "jpg", new File("result.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 
-    private List<Vector<Double>> buildCodebook(List<Vector<Double>> initialVectors, Integer requiredSize) {
+    private Vector<Vector<Integer>> getVector(int index, Vector<Vector<Vector<Integer>>> codebook) {
+        return codebook.get(index);
+    }
+
+    private Vector<Vector<Vector<Integer>>> buildCodebook(Vector<Vector<Vector<Integer>>> initialVectors, Integer requiredSize) {
 
         Integer numVectors = initialVectors.size();
-        List<Vector<Double>> codebook = new ArrayList<>();
+        Vector<Vector<Vector<Integer>>> codebook = new Vector<>();
 
         // calculate first average vector
-        Vector<Double> codebookInitialVector = new Vector<>();
-        for (int i = 0; i < initialVectors.get(0).size(); i += 1) {
-            Double avg = 0.0;
-            for (int j = 0; j < numVectors; j += 1) {
-                avg += initialVectors.get(j).get(i);
+        Vector<Vector<Integer>> codebookInitialVector = new Vector<>();
+        for (int i = 0; i < 2; i++) {
+            Vector<Integer> currentAverage = new Vector<>();
+            for (int j = 0; j < 2; j++) {
+                int avg = 0;
+                for (int k = 0; k < numVectors; k++) {
+                    avg += initialVectors.get(k).get(i).get(j);
+                }
+                currentAverage.add(avg / numVectors);
             }
-            codebookInitialVector.add(avg / numVectors);
+            codebookInitialVector.add(currentAverage);
         }
         codebook.add(codebookInitialVector);
 
         // loop till filling codebook
         while (codebook.size() < requiredSize) {
 
-            List<Vector<Double>> updatedCodebook = new ArrayList<>();
+            Vector<Vector<Vector<Integer>>> updatedCodebook = new Vector<>();
 
             // do the splitting for every codebook vector
             for (int i = 0; i < codebook.size(); i += 1) {
-                updatedCodebook.add(splitVector(codebook.get(i), -1.0));
-                updatedCodebook.add(splitVector(codebook.get(i), 1.0));
+                Vector<Vector<Integer>> code1 = new Vector<>() , code2 = new Vector<>();
+                for(int j = 0 ; j < codebook.get(i).size() ; j++){
+                    Vector<Integer> temp1 = new Vector<>();
+                    Vector<Integer> temp2 = new Vector<>();
+                    for(int k = 0 ; k < codebook.get(i).get(0).size() ; k++){
+                        temp1.add((int) Math.floor(codebook.get(i).get(j).get(k)));
+                        temp2.add((int) (Math.floor(codebook.get(i).get(j).get(k))+1));
+
+                    }
+                    code1.add(temp1);
+                    code2.add(temp2);
+                }
+                updatedCodebook.add(code1);
+                updatedCodebook.add(code2);
             }
 
-            List<Vector<Double>> updatedCodebook2 = new ArrayList<>();
+            Vector<Vector<Vector<Integer>>> updatedCodebook2 = new Vector<>();
 
-            List<Vector<Vector<Double>>> quantized = quantize(initialVectors, updatedCodebook);
+            Vector<Vector<Vector<Vector<Integer>>>> quantized = quantize(initialVectors, updatedCodebook);
 
             // calculate averages for every matched group separately
             for (int i = 0; i < quantized.size(); i++) {
 
-                Vector<Double> codeBookVector = new Vector<>();
-                for (int j = 0; j < quantized.get(i).get(0).size(); j += 1) {
-                    Double avg = 0.0;
-                    for (int k = 0; k < quantized.get(i).size(); k++) {
-                        avg += quantized.get(i).get(k).get(j);
+                Vector<Vector<Integer>> codeBookVector = new Vector<>();
+                for (int j = 0; j < 2; j++) {
+                    Vector<Integer> currentAverage = new Vector<>();
+                    for (int k = 0; k < 2; k++) {
+                        int avg = 0;
+                        for (int l = 0; l < quantized.get(i).size(); l++) {
+                            avg += quantized.get(i).get(l).get(j).get(k);
+                        }
+                        currentAverage.add(avg / quantized.get(i).size());
                     }
-                    codeBookVector.add(avg / quantized.get(i).size());
+                    codeBookVector.add(currentAverage);
                 }
                 updatedCodebook2.add(codeBookVector);
 
-            }
-
-            codebook = updatedCodebook;
-        }
-
-        List<Vector<Double>> updatedCodebook2;
-
-        // loop till no changed groups
-        while (true) {
-
-            updatedCodebook2 = new ArrayList<>();
-
-            List<Vector<Vector<Double>>> quantized = quantize(initialVectors, codebook);
-
-            // calculate averages for every matched group separately
-            // same as previous loop
-            for (int i = 0; i < quantized.size(); i++) {
-
-                Vector<Double> codeBookVector = new Vector<>();
-                for (int j = 0; j < quantized.get(i).get(0).size(); j += 1) {
-                    Double avg = 0.0;
-                    for (int k = 0; k < quantized.get(i).size(); k++) {
-                        avg += quantized.get(i).get(k).get(j);
-                    }
-                    codeBookVector.add(avg / quantized.get(i).size());
-                }
-                updatedCodebook2.add(codeBookVector);
-
-            }
-
-            // stop when no change
-            if (codebook.equals(updatedCodebook2)) {
-                break;
             }
 
             codebook = updatedCodebook2;
         }
 
-        return updatedCodebook2;
+        Vector<Vector<Vector<Integer>>> updatedCodebook2;
+
+        // loop till no changed groups
+//        while (true) {
+//            updatedCodebook2 = new Vector<>();
+//
+//            Vector<Vector<Vector<Vector<Integer>>>> quantized = quantize(initialVectors, codebook);
+//
+//            // calculate averages for every matched group separately
+//            // same as previous loop
+//            for (int i = 0; i < quantized.size(); i++) {
+//
+//                Vector<Vector<Integer>> codeBookVector = new Vector<>();
+//                for (int j = 0; j < 2; j++) {
+//                    Vector<Integer> currentAverage = new Vector<>();
+//                    for (int k = 0; k < 2; k++) {
+//                        int avg = 0;
+//                        for (int l = 0; l < quantized.get(i).size(); l++) {
+//                            avg += quantized.get(i).get(l).get(j).get(k);
+//                        }
+//                        currentAverage.add(avg / quantized.get(i).size());
+//                    }
+//                    codeBookVector.add(currentAverage);
+//                }
+//                updatedCodebook2.add(codeBookVector);
+//
+//            }
+//
+//            // stop when no change
+//            if (codebook.equals(updatedCodebook2)) {
+//                break;
+//            }
+//
+//            codebook = updatedCodebook2;
+//        }
+
+        return codebook;
     }
 
-    private Vector<Double> splitVector(Vector<Double> initialVector, Double scale) {
-        Vector<Double> split = new Vector<>(initialVector.size());
-        for (int i = 0; i < initialVector.size(); i++) {
-            if (initialVector.get(i) % 1 == 0) {
-                split.add(initialVector.get(i) + (scale));
-            } else if (scale > 0) {
-                split.add(Math.ceil(initialVector.get(i)));
-            } else if (scale < 0) {
-                split.add(Math.floor(initialVector.get(i)));
-            }
-        }
-        return split;
-    }
-
-    private List<Vector<Vector<Double>>> quantize(List<Vector<Double>> inputVectors, List<Vector<Double>> codebook) {
+    private Vector<Vector<Vector<Vector<Integer>>>> quantize(Vector<Vector<Vector<Integer>>> inputVectors, Vector<Vector<Vector<Integer>>> codebook) {
 
         // group vectors in a vector with index equal to its index in codebook
 
-        List<Vector<Vector<Double>>> quantizedIndices = new ArrayList<>(codebook.size());
+        Vector<Vector<Vector<Vector<Integer>>>> quantizedIndices = new Vector<>(codebook.size());
 
         for (int i = 0; i < codebook.size(); i++) {
             quantizedIndices.add(new Vector<>());
         }
 
         // do the vector-codebook matching
-        for (Vector<Double> inputVector : inputVectors) {
+        for (Vector<Vector<Integer>> inputVector : inputVectors) {
+
             int bestIndex = findClosestCodebookVector(inputVector, codebook);
             quantizedIndices.get(bestIndex).add(inputVector);
+
         }
         return quantizedIndices;
     }
 
-    private Integer findClosestCodebookVector(Vector<Double> inputVector, List<Vector<Double>> codebook) {
+    private Integer findClosestCodebookVector(Vector<Vector<Integer>> inputVector, Vector<Vector<Vector<Integer>>> codebook) {
         // find index of best matching codebook vector to the input vector
         Integer bestIndex = 0;
-        Double bestDistance = Double.MAX_VALUE;
+        Integer bestDistance = Integer.MAX_VALUE;
         for (int i = 0; i < codebook.size(); i++) {
-            Double distance = calculateDistance(inputVector, codebook.get(i));
+            Integer distance = calculateDistance(inputVector, codebook.get(i));
             if (distance < bestDistance) {
                 bestDistance = distance;
                 bestIndex = i;
@@ -205,23 +277,20 @@ public class Quantizer {
         return bestIndex;
     }
 
-    private Double calculateDistance(Vector<Double> vector1, Vector<Double> vector2) {
-        Double sum = 0.0;
+    private Integer calculateDistance(Vector<Vector<Integer>> vector1, Vector<Vector<Integer>> vector2) {
+        int sum = 0;
         for (int i = 0; i < vector1.size(); i++) {
-            sum += Math.pow(vector1.get(i) - vector2.get(i), 2);
+            for(int j = 0 ;  j < vector1.size(); j++) {
+                sum +=(int) Math.pow(vector1.get(i).get(j) - vector2.get(i).get(j), 2);
+            }
         }
-        return Math.sqrt(sum);
+        return (int) Math.sqrt(sum);
     }
 
     public static void main(String[] args) throws IOException {
-//        String path = System.getProperty("user.dir") + "/compression" + ".bin";
-//        Quantizer quantizer = new Quantizer();
-//        quantizer.compress("input.txt", path);
-//        quantizer.decompress(path);
-
         Quantizer quantizer = new Quantizer();
-        quantizer.compress("input.txt", "compression.bin");
-        quantizer.decompress("compression.bin", "decompression.txt");
+        quantizer.compress("img1.png", "compression.bin");
+//        quantizer.decompress("compression.bin", "decompression.txt");
     }
 
     public void decompress(String compressedFilePath, String decompressedFilePath) throws IOException {
